91e07e3ccbba970f4e447ed438f657976e65ccbf
arty <slava.arty@gmail.com>
2010-09-02 12:51:54 +0400

    add button's action

--- DropSendClient/mainwindow.h
+++ DropSendClient/mainwindow.h
@@ -19,6 +19,9 @@ private:
     Ui::MainWindow *ui;
 
 private slots:
+    void on_pushButton_4_clicked();
+    void on_pushButton_3_clicked();
+    void on_pushButton_clicked();
     void on_pushButton_7_clicked();
     void on_pushButton_2_clicked();
     void on_pushButton_9_clicked();
